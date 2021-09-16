c0c0735fae881176eb9683afa8e2a746436ac340
Yuri Stephano <=>
2021-09-16 18:17:09 -0300

    animacao ao scroll refeito

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -2,24 +2,44 @@ export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
-
-    this.animaScroll = this.animaScroll.bind(this);
+    // Método bind para fazer referência ao proprio objeto/classe(ScrollAnima)
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.windowMetade) < 0;
-      if (isSectionVisible) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+  // Retorna um objeto com uma distância diferente de cada section em relação ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  // Verifica as distâncias de cada section da pagina
+  // e realiza uma verificação, caso for true, uma animação ocorrerá
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if(window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if(this.sections.length){
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  // Remove o evento de scroll e pausa as animações
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
