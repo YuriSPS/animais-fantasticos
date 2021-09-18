24b04b304dbbd9926eb1a75682d012ce6301bd56
Yuri Stephano <=>
2021-09-18 12:41:13 -0300

    corrigido o caminho da api animais.json

--- js/script.js
+++ js/script.js
@@ -38,5 +38,5 @@ const funcionamento = new Funcionamento('[data-semana]');
 funcionamento.init();
 
 // Importações com API
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
